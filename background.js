0c1698c1b43c683000dfa5636c12abef272c59e1
ezdub <ezdub@users.noreply.github.com>
2025-02-12 14:31:26 -1000

    manifest v3 updates

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+import browser from './browser-polyfill.js';
+
 browser.runtime.onInstalled.addListener(function (details) {
   //reload gmail upon extension "install", "update", "chrome_update" (details.OnInstalledReason)
   browser.tabs.query({ url: 'https://calendar.google.com/calendar/*' }).then(function (tabs) {
